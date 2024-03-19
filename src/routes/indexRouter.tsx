a1234992ec017c164d203193bf031b642f817eb3
Vakiyama <vakiyama@outlook.com>
2024-03-19 14:48:41 -0700

    fix package.json

--- src/routes/indexRouter.tsx
+++ src/routes/indexRouter.tsx
@@ -1,5 +1,14 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import { html } from '@elysiajs/html';
 import { Home } from '../pages/Home';
 
-export const indexRouter = new Elysia().use(html()).get('/', () => <Home />);
+export const indexRouter = new Elysia()
+  .get('/', () => <Home />)
+  .post('/test', ({ body }) => {
+    body.password
+    }, {
+    body: t.Object({
+      username: t.Number(),
+      password: t.String(),
+    }),
+  });
